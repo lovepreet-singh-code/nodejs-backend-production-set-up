32bde224ff8fce0291b6fc7aaf2fd12004c7c0c8
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2025-01-27 19:56:45 +0530

    feat: Add MongoDB setup

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,34 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import app from './app'
 import config from './config/config'
 import logger from './util/logger'
-
+import databaseService from './service/databaseService'
 
 const server = app.listen(config.PORT)
 
-
-;(() => {
+;(async () => {
     try {
         // Database Connection
-        
-  
+        const connection = await databaseService.connect()
         logger.info(`DATABASE_CONNECTION`, {
+            meta: {
+                CONNECTION_NAME: connection.name
+            }
+        })
+
+        
+
+        logger.info(`APPLICATION_STARTED`, {
             meta: {
                 PORT: config.PORT,
                 SERVER_URL: config.SERVER_URL
             }
         })
     } catch (err) {
-       
         logger.error(`APPLICATION_ERROR`, { meta: err })
 
         server.close((error) => {
             if (error) {
-             
                 logger.error(`APPLICATION_ERROR`, { meta: error })
             }
 
